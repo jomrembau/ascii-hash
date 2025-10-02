9eb3807e79f26fd074e28aa056abf49e34cab011
jomrembau <jomir.bautista@gmail.com>
2025-10-02 13:18:00 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+password = "passwort"
+block1 = list(password[:len(password)//2])
+block2 = list(password[len(password)//2:])
+multiplier = []
+mul = 1
+total = 0
+sum_list = []
+
+for i in range(len(block1)):
+    if i % 4 == 0:
+        mul = 1
+    else:
+        mul = mul * 64
+    multiplier.append(mul)
+    total += ord(block1[i]) * mul
+    sum_list.append(total)
+
+for x, y, z in zip(block1, multiplier, sum_list):
+    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {z}")
+
+total_block2 = total
+for x, y in zip(block2, multiplier):
+    total_block2 += ord(x) * y
+    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {total_block2}")
