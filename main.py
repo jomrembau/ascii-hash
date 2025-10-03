becde911044985437d723a7bfc21d09a6eb3a107
jomrembau <jomir.bautista@gmail.com>
2025-10-03 10:27:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import hashlib
+
 password = "passwort"
 block1 = list(password[:len(password)//2])
 block2 = list(password[len(password)//2:])
@@ -5,6 +7,9 @@ multiplier = []
 mul = 1
 total = 0
 sum_list = []
+
+encoded_password = password.encode()
+hash_value = hashlib.sha256(encoded_password)
 
 for i in range(len(block1)):
     if i % 4 == 0:
@@ -16,9 +21,13 @@ for i in range(len(block1)):
     sum_list.append(total)
 
 for x, y, z in zip(block1, multiplier, sum_list):
-    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {z}")
+
+    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {z} | Hexadecimal: {hex(z)}")
 
 total_block2 = total
-for x, y in zip(block2, multiplier):
+for x, y, z in zip(block2, multiplier, sum_list):
     total_block2 += ord(x) * y
-    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {total_block2}")
+    print(f"{x} | Ascii {ord(x)} | M {y} | Ascii*M: {ord(x)*y} | Hash: {total_block2} "
+          f"| Hexadecimal: {hex(total_block2)}")
+
+
